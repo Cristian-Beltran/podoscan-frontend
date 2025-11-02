378802a237a98f72825809b76d19830da37cba04
cristian <cristian.beltran.paco@gmail.com>
2025-11-01 20:14:24 -0400

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
-    host: true, // ó '0.0.0.0'
+    host: true, // escucha en 0.0.0.0
     port: 5173,
-    strictPort: true,
+    allowedHosts: [".local"],
   },
 });
